c029dbde50ca67dc901d25ed3b17c0bbc5089764
Johannes Weber <johannes@drweber.de>
2024-01-02 19:02:15 -0800

    adjustment in constants

--- backend/constants.js
+++ backend/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-    baseUrl: `${process.env.HOST}:${process.env.PORT}` ?? `http://jj-photos.wnet.wn:3020`,
+    baseUrl: process.env.HOST && process.env.PORT ? `${process.env.HOST}:${process.env.PORT}` : `http://jj-photos.wnet.wn:3020`,
     defaultCollectionName: 'photos',
 };
 
